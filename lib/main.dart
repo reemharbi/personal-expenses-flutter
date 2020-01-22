7cce3327f55582d98c0fafdec260506f8109d3b2
reemharbi <reemharbi.3@gmail.com>
2020-01-22 15:23:50 +0300

    Show transactions

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.deepOrange,
-            child: Text('LIST OF TX'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
           ),
         ],
       ),
