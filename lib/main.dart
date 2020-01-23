e6f32b2ed37b8955cbc57ea5cc47f55c0fa23e1e
reemharbi <reemharbi.3@gmail.com>
2020-01-23 12:01:26 +0300

    Seperation of concerns + SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import 'package:personal_expenses/models/transaction.dart';
+
+import './widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,21 +16,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Watch',
-      amount: 99.99,
-      date: DateTime.now(),
-    ),
-  ];
-
   String titleInput;
   String amountInput;
 
@@ -39,92 +25,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              color: Colors.yellow,
-              child: Text('CHART'),
-              elevation: 5,
-            ),
-          ),
-          Card(
-            child: Container(
-              padding: EdgeInsets.all(10),
+      body: SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Purchased item'),
-                    onChanged: (val) => titleInput = val,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    onChanged: (val) => amountInput = val,
-                  ),
-                  FlatButton(
-                    child: Text('Add'),
-                    textColor: Colors.purple,
-                    onPressed: () {
-                      print(titleInput);
-                      print(amountInput);
-                    },
-                  )
-                ],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.yellow,
+                child: Text('CHART'),
+                elevation: 5,
               ),
             ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(15),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(tx.title,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                elevation: 5,
-              );
-            }).toList(),
-          ),
-        ],
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }
